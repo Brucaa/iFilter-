d216080495e6ca063ef37e2f13827a77f9017aca
Milos Jovanovic <Milos-20@hotmail.com>
2019-12-09 10:07:56 +0100

    Initial Commit

---
+++ iFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iFilter
+//
+//  Created by Milos Jovanovic on 09/12/2019.
+//  Copyright © 2019 Milos Jovanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
